b86527a48b7010dfd333f25f9378d84866cb7ebb
AlexRoac <alejandro.roac05@gmail.com>
2024-08-30 22:50:10 -0600

    changos

---
+++ code-register.php
@@ -0,0 +1,8 @@
+<?php
+    
+    // Incluir archivo de conexion a la base de datos
+    require_once "conexion.php";
+    $id = $username = $email = $password = "";
+    $id_err = $username_err = $email_err = $password_err = "";
+
+?>
